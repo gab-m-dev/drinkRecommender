aac251fcb4a768303a61d97c706dc918b1423271
Gabor Muff <gabor.muff99@gmail.com>
2022-03-06 14:01:10 +0100

    Anpassungen GiIgnore

--- api-drinks/src/local-example.php
+++ api-drinks/src/local-example.php
@@ -1,5 +1,4 @@
 <?php
-
 return array(
     'db' => ['user' => 'test', 'password' => 'test'],
     'user' => ['id' => 1, 'username' => 'test', 'password' => 'testhash'],
--- api-drinks/src/local.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-    'db' => ['user' => 'MariaUser', 'password' => 'MariaUserpw'],
-    'user' => ['id' => 1, 'username' => 'myuser', 'password' => '$2y$10$FmqN/VUIIsVES7Y5DB5fpeY4lyjn2WGhEuAARObRklijlrSnyEwoq'],
-    'secret' => 'zbB7C4dq9tWYFyCGn4iUGrbzOPX4AWUmJEjvZAdS5lto0'
-);
