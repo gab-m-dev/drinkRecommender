5aab0a2e619ae92c9cf67feb528a912b445ea7b4
gab-m-dev <76039747+gab-m-dev@users.noreply.github.com>
2022-03-08 23:58:53 +0100

    Update index.php

--- api-drinks/index.php
+++ api-drinks/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://drinks.gabormuff.info");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-type: text/plain; charset=utf-8');
